b09f8fa74d8ca5da2ab78c96a02747c8fbbc37e1
michaellennox <michaelctlennox@gmail.com>
2016-02-22 12:45:50 +0000

    Passes view_menu test, adds price_test for Menu

--- app/menu.py
+++ app/menu.py
@@ -1,2 +1,10 @@
+import json
+
+
 class Menu(object):
+    def __init__(self, menu):
+        data = json.load(open(menu))
+        self._menu = data[0]['prices'][0]
+
     def view_menu(self):
+        return self._menu
--- tests/menu_tests.py
+++ tests/menu_tests.py
@@ -11,3 +11,9 @@ class TestMenu(object):
 
     def test_view_menu_returns_the_menu(self):
         eq_(self.menu.view_menu(), self.price_data)
+
+    def test_price_returns_the_price_of_item_requested(self):
+        eq_(self.menu.price('Cafe Latte'), self.price_data['Cafe Latte'])
+
+    def test_price_raises_exception_if_item_is_not_in_menu(self):
+        pass
